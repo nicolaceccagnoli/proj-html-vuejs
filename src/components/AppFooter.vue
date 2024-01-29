c9e49e03fb06aa87da01cba9660bff6d0ff2a487
MironAlberto <alberto.miron19@gmail.com>
2024-01-29 20:04:41 +0100

    Started working on the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,8 +2,67 @@
     export default {
         data() {
             return {
-
-            };
+                footerTopCompany: {
+                    company: [
+                        "Company",
+                        {
+                            text: 'Institutional'
+                        },
+                        {
+                            text: 'Social & Events'
+                        },
+                        {
+                            text: 'Innovation'
+                        },
+                        {
+                            text: 'Environment'
+                        },
+                        {
+                            text: 'Technology'
+                        }
+                    ]
+                },
+                footerTopServices: {
+                    services: [
+                        "Services",
+                        {
+                            text: 'Audit & Assurance'
+                        },
+                        {
+                            text: 'Financial Advisory'
+                        },
+                        {
+                            text: 'Analytics M&A'
+                        },
+                        {
+                            text: 'Middle Marketing'
+                        },
+                        {
+                            text: 'Legal Consulting'
+                        }
+                    ]
+                },
+                footerTopConsultants: {
+                    consultants: [
+                        "Consultants",
+                        {
+                            text: 'David Cooper'
+                        },
+                        {
+                            text: 'Oliver Jones'
+                        },
+                        {
+                            text: 'Emma Lopez'
+                        },
+                        {
+                            text: 'T. Johnson'
+                        },
+                        {
+                            text: 'Jacob Hill Jr'
+                        }
+                    ]
+                }
+            }
         },
         methods: {
 
@@ -14,13 +73,193 @@
 
 <template>
     <footer>
-        <div class="my-container">
-            FOOTER
+        <div class="footer-top">
+            <div class="my-container">
+                <div class="row">
+                    <div class="col">
+                        <div class="footer-card d-flex flex-column text-secondary">
+                            <div class="img-box">
+                                <img src="../../public/logo.PNG" alt="nexgen-logo">
+                            </div>
+                            <p class="p-1 text-secondary">
+                                Built to the smallest detail with
+                                a focus on feature availability.
+                            </p>
+                            <ul class="fa-ul ps-4">
+                                <li><span class="fa-li"><i class="fa-solid fa-phone"></i></span>+1 (305) 1234-5678</li>
+                                <li><span class="fa-li"><i class="fa-solid fa-envelope"></i></span>hello@example.com</li>
+                                <li><span class="fa-li"><i class="fa-solid fa-location-dot"></i></span>Main Avenue, 987</li>
+                            </ul>
+                            <button class="btn ps-3">
+                                BUY NEXGEN
+                            </button>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="footer-card bg-color">
+                            <div class="title" v-for="(elem, index) in footerTopCompany" :key="index">
+                                <h4>
+                                    {{ elem[0] }}
+                                </h4>
+                                <ul>
+                                    <li v-for="(elem, index) in elem.slice(1)" :key="index">
+                                        <a href="#">
+                                            {{ elem.text }}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="footer-card bg-color">
+                            <div class="title" v-for="(elem, index) in footerTopServices" :key="index">
+                                <h4>
+                                    {{ elem[0] }}
+                                </h4>
+                                <ul>
+                                    <li v-for="(elem, index) in elem.slice(1)" :key="index">
+                                        <a href="#">
+                                            {{ elem.text }}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="footer-card bg-color">
+                            <div class="title" v-for="(elem, index) in footerTopConsultants" :key="index">
+                                <h4>
+                                    {{ elem[0] }}
+                                </h4>
+                                <ul>
+                                    <li v-for="(elem, index) in elem.slice(1)" :key="index">
+                                        <a href="#">
+                                            {{ elem.text }}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="my-container">
+                <div class="footer-bottom-left">
+                    <p>
+                        2023 - All rights reserved. Nexgen is maintained by Codings Group.
+                    </p>
+                </div>
+                <div class="footer-bottom-right">
+                    <ul>
+                        <li>
+                            <i class="fa-brands fa-facebook"></i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-twitter"></i>
+                        </li>
+                        <li>
+                            <i class="fa-brands fa-linkedin"></i>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
 
 <style lang="scss" scoped>
 @import '../assets/scss/partials/variables.scss';
+footer{
+    width: 100%;
+
+    .footer-top{
+        height: 700px;
+        background-color: #111117;
+        display: flex;
+        align-items: center;
+
+        .row{
+            gap: 8px
+        }
+
+        .bg-color{
+            background-color: #17171C;
+        }
+        .footer-card{
+            border-radius: 10px;
+            padding: 15px;
+            display: flex;
+            justify-content: space-between;
+
+            .img-box{
+                width: 150px;
+                padding-bottom: 10px;
+
+                img{
+                    width: 100%;
+                }
+            }
+
+            button{
+                color: lightgray;
+                border: 1px solid $color-five;
+                border-radius: 20px;
+                max-width: 150px;
+                padding: 8px 20px;
+                margin: 20px 0;
+
+                &:hover{
+                    border: 1px solid white;
+                    background-color: #0E0E14;
+                }
+            }
+
+            h4{
+                color: white;
+                padding: 25px 10px;
+                font-weight: bold;
+            }
+
+            ul{
+                list-style: none;
+
+                li{
+                    padding: 7px 18px;
+                    a{
+                        color: gray;
+                        text-decoration: none;
+                        list-style: none;
+
+                        &:hover{
+                            color: $color-five;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    .footer-bottom{
+        height: 100px;
+        background-color: #16161C;
+        display: flex;
+        justify-content: space-between;
+
+        ul{
+            list-style: none;
+
+            li{
+                display: inline-block;
+                padding-left: 5px;
+            }
+        }
+    }
+}
+
+
 
 </style>
