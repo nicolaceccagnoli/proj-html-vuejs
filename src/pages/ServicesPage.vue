d855712dd5548aaa9b0c58c7a5ae369beef5aa5b
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-30 23:01:54 +0100

    card-service-rework

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -79,80 +79,63 @@
 
     <!--cards-1-->
 
-    <div id="container-card">
-        <div class="container">
-            <div v-for="(card, i) in serviceCards"
-                :key="i" class="row d-flex justify-content-center pt-5 pb-5">
-                <div>
-                    <div class="col-3">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="icon">
-                                    {{ card.path }}
-                                </div>                                
-                            </div>
-                        </div>
-                        
-                        <div class="col-12">
-                            {{ card.title }}
-                        </div>
-                        <div class="col-12">
-                            {{ card.text }}
-                        </div>
+    <div id="container-card-1">
+        <div class="my-container">
+            <div class="row">
+                <div v-for="(card, i) in serviceCards" class="single-card col-4">
+                    <div> {{ card.path }}</div>
+                    <div>
+                        <h2>{{ card.title }}</h2>
+                        <p>{{ card.text }}</p>
                     </div>
                 </div>
-                                    
-            </div> 
+            </div>
         </div>
     </div>
 
     <!--cards-2-->
 
     <div id="container-card2">
-        <div class="container">
-            <div class="row text-center">
+        <div class="my-container text-center">
+            <div class="row d-flex">
                 <div class="col-12">
-                    <h5>OUR BUSINESS AREAS</h5>
+                    <h5>
+                        OUR BUSINESS AREAS
+                    </h5>
                 </div>
                 <div class="col-12">
-                    <h1>Excellence in Services</h1>
+                    <h2>Excellence in Services</h2>
                 </div>
                 <div class="col-12">
                     <p>
-                        We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowned professionals.
-                        These are some options that you can hire.
+                        We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowned professionals.These are some options that you can hire.
                     </p>
                 </div>
             </div>
+
             <div class="row">
-                <div
+                <div 
                 v-for="(card2, j) in serviceCards2"
-                :key="j">
-                    <div class="col-6" style="border: 1px solid black;">
-                        <div class="row">
-                            <div class="col-10">
-                                {{ card2.img}}
-                            </div>
-                            <div class="col-2">
-                                {{ card2.icon }}
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <h3>{{card2.name}}</h3>
-                            </div>
-                            <div class="col-12">
-                                <p> {{ card2.text }}</p>
-                            </div>
-                        </div>
-                    </div>               
-                </div>                    
-            </div>                            
+                :key="j" class="col-6 single-card2">
+                    <div class="container-up d-flex justify-content-between">
+                         <div class="icon"></div>
+                         <div class="icon"></div>
+                    </div>
+                    <div class="container-button">
+                        <h2 class="d-flex">
+                            {{ card2.name }}
+                        </h2>
+                        <h5 class="text-left">
+                           wjk
+                        </h5>
+                    </div>
+                    
+                </div>
+                
+            </div>
         </div>
     </div>
 
-        
-    
 </template>
 
 <style lang="scss" scoped>
@@ -175,15 +158,23 @@
     background-color: $color-three;
 }
 
-#card {
-    .icon {
-        margin: 20px 0;
-    }
+.single-card {
+    width: calc((100% / 3) - 20px);
+    border-radius: 10px;
+    border: 1px solid black;
+    padding: 20px;
+    margin: 10px;
 
-    p {
-        padding: 20px 0;
+    h2 {
+        margin-bottom: 20px;
     }
 }
+
+.single-card > * {
+    padding: 10px;
+}
+
+
 
 .col-3 {
     background-color: white;
@@ -193,4 +184,14 @@
 
 //card2
 
+.single-card2 {
+    width: calc((100% / 2) - 20px);
+    border: 1px solid black;
+    padding: 20px;
+    margin: 10px;
+
+    h2 {
+        margin-bottom: 20px;
+    }
+}
 </style>
