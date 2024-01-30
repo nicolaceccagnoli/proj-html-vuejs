1c2e9d97b9b9356e83259ad3e9284c43eb26e2a4
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-30 19:05:49 +0100

    Add card service

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -2,7 +2,44 @@
     export default {
         data() {
             return {
-
+                serviceCards: [
+                {
+                    img:'',
+                    name:'Audit e Garanzia',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                },
+                {
+                    img:'',
+                    name:'Consulente finanziario',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                },
+                {
+                    img:'',
+                    name:'Analisi e fusioni e acquisizioni ',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                },
+                {
+                    img:'',
+                    name:'Marketing medio',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                },
+                {
+                    img:'',
+                    name:'Consulenza legale',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                },
+                {
+                    img:'',
+                    name:'Rischio normativo',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
+                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                }
+                ]
             };
         }
     }
@@ -10,6 +47,7 @@
 
 
 <template>
+
     <div id="header-service">
         <!--header-->
         <div class="d-flex justify-content-center">
@@ -20,31 +58,83 @@
         </div>
     </div>
 
+    <!--cards-1-->
+
+    <div id="container-card">
         <div class="container">
-            <div class="row">
-                <div class="col-4">
+            <div class="row d-flex justify-content-center pt-5 pb-5">
+
+                <div class="col-3">
                     <div id="card" class="container">
-                        <div class="icon">icon</div>
+                        <div class="icon"><i class="fa-solid fa-users"></i></div>
                         <h4>capitale umano</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis libero error non animi, quae, ipsam,</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis </p>
                     </div>
                 </div>
-                <div class="col-4">
+
+                <div class="col-3">
                     <div id="card" class="container">
-                        <div class="icon">icon</div>
+                        <div class="icon"><i class="fa-solid fa-chart-pie"></i></div>
                         <h4>capitale umano</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis libero error non animi, quae, ipsam,</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis</p>
                     </div>
                 </div>
-                <div id="card" class="col-4">
+
+                <div id="card" class="col-3">
                     <div class="container">
-                        <div class="icon">icon</div>
+                        <div class="icon"><i class="fa-solid fa-gauge"></i></div>
                         <h4>capitale umano</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis libero error non animi, quae, ipsam,</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente eius nobis libero</p>
                     </div>
                 </div>
+
             </div> 
         </div>
+    </div>
+
+    <!--cards-2-->
+
+    <div id="container-card2">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h5>le nostre aree di business</h5>
+                </div>
+                <div class="col-12">
+                    <h1>eccellenza nei servizi</h1>
+                </div>
+                <div class="col-12">
+                    <p>siamo leader nella fornitura di servizi di consulenza con un insieme di tecnlogie
+                        all'avanguardia e un team di professionisti esperti e rinomati. Questo sono alcune opzioni che puoi assumere.
+                    </p>
+                </div>
+            </div>
+            <div class="row d-flex">
+                <div
+                v-for="(card, i) in serviceCards"
+                :key="i">
+                    <div class="col-6" style="border: 1px solid black;">
+                        <div class="row">
+                            <div class="col-10">img</div>
+                            <div class="col-2">
+                                {{ card.icon }}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h3>{{card.name}}</h3>
+                            </div>
+                            <div class="col-12">
+                                <p> {{ card.text }}</p>
+                            </div>
+                        </div>
+                    </div>               
+                </div>                    
+            </div>                            
+        </div>
+    </div>
+
+        
     
 </template>
 
@@ -62,17 +152,28 @@
     border: 1px dashed black;
 }
 
+//card
+
+#container-card {
+    background-color: $color-three;
+}
+
 #card {
     .icon {
-        margin: 10px 0;
-    }
-
-    h4 {
-        margin: 10px 0;
+        margin: 20px 0;
     }
 
     p {
-        margin: 10px 0;
+        padding: 20px 0;
     }
 }
+
+.col-3 {
+    background-color: white;
+    margin: 10px;
+    border-radius: 10px;
+}
+
+//card2
+
 </style>
