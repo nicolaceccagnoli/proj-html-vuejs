5151ad7d2df79f0083ea8d5e003491cf9cd25c2a
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-31 15:54:50 +0100

    service#2

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -6,95 +6,133 @@
                 {                
                     title:'Human Capital',
                     text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
-                    path: `<i class="fa-solid fa-users"></i>`
+                    path: `bi bi-people-fill`
                 },
                 {
                     title:'Core Business',
                     text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
-                    path: `<i class="fa-solid fa-chart-pie">`
+                    path: `bi bi-pie-chart`
                 },
                 {
                     title:'Performance',
                     text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aut ab po',
-                    path: `<i class="fa-solid fa-gauge"></i>`
+                    path: 'bi bi-speedometer'
                 }
             
                 ],
 
                 serviceCards2: [
                 {
-                    img:'/public/svgs/svg-1.svg',
+                    img:'/svgs/svg-1.svg',
                     name:'Audit & Assurance',
                     text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
                 },
                 {
-                    img:'/public/svgs/svg-2.svg',
+                    img:'/svgs/svg-2.svg',
                     name:'Financial Advisory',
                     text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
                 },
                 {
-                    img:'/public/svgs/svg-3.svg',
+                    img:'/svgs/svg-3.svg',
                     name:'Analytics and M&Q',
                     text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
                 },
                 {
-                    img:'/public/svgs/svg-4.svg',
+                    img:'/svgs/svg-4.svg',
                     name:'Middle Marketing',
                     text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
                 },
                 {
-                    img:'/public/svgs/svg-5.svg',
+                    img:'/svgs/svg-5.svg',
                     name:'Legal Consulting',
                     text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
                 },
                 {
-                    img:'/public/svgs/svg-6.svg',
-                    name:'Regular Risk',
-                    text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-                    path: `<i class="fa-solid fa-arrow-right"></i>`
+                    img:'/svgs/svg-6.svg',
+                    name:'Regular Risk',                    
+                    text:'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',                            
                 }
                 ],
                 serviceCards3: [
                 {                
                     title:'Standard',
                     price:'$ 490/mo',
+                    icon: `bi bi-rocket-takeoff`,
                     list: [
-                        'Standard Accounting',
-                        'Platform Access',
-                        'business Orientation',
-                        'Dedicated Consultant',
-                        'Personal Assistence'
+                        {
+                        option:'Standard Accounting',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Platform Access',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Personal Assistence',
+                        check: 'bi bi-x'
+                    },
+                    {
+                        option:'business Orientation',
+                        check: 'bi bi-x'
+                    },
+                    {
+                        option:'Dedicated Consultant',
+                        check: 'bi bi-x'
+                    }                        
                     ],
-                    icon1: '',
-                    icon2: '',
 
                 },
                 {                
                     title:'Professional',
                     price:'$ 890/mo',
-                    list: [
-                        'Standard Accounting',
-                        'Platform Access',
-                        'business Orientation',
-                        'Dedicated Consultant',
-                        'Personal Assistence'
-                    ]
+                    icon: `bi bi-briefcase-fill`,
+                    list: [{
+                        option:'Standard Accounting',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Platform Access',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Personal Assistence',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'business Orientation',
+                        check: 'bi bi-x'
+                    },
+                    {
+                        option:'Dedicated Consultant',
+                        check: 'bi bi-x'
+                    }                        
+                    ],
                 },
                 {                
                     title:'ultimate',
                     price:'$ 1390/mo',
-                    list: [
-                        'Standard Accounting',
-                        'Platform Access',
-                        'business Orientation',
-                        'Dedicated Consultant',
-                        'Personal Assistence'
-                    ]
+                    icon: `class="bi bi-gem`,
+                    list: [{
+                        option:'Standard Accounting',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Platform Access',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Personal Assistence',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'business Orientation',
+                        check: 'bi bi-check'
+                    },
+                    {
+                        option:'Dedicated Consultant',
+                        check: 'bi bi-check'
+                    }                        
+                    ],
                 },
                 ]
             };
@@ -124,7 +162,7 @@
                 class="single-card col-4">
                     <div> {{ card.path }}</div>
                     <div>
-                        <h2>{{ card.title }}</h2>
+                        <h2>{{ card.list }}</h2>
                         <p>{{ card.text }}</p>
                     </div>
                 </div>
@@ -163,13 +201,17 @@
                 :key="j"
                 class="col-6 single-card2">
                     <div class="container-up d-flex justify-content-between">
-                         <div class="icon"></div>
-                         <div class="icon"></div>
+                         <div class="icon filter">
+                            <img :src="card2.img" :alt="card2.name">
+                         </div>
+                         <div>
+                            <i class="bi bi-arrow-right-short"></i>
+                        </div>
                     </div>
                     <div class="container-button">
-                        <h2 class="d-flex">
+                        <h3 class="d-flex">
                             {{ card2.name }}
-                        </h2>
+                        </h3>
                         <p class="d-flex">
                            {{card2.text}}
                         </p>
@@ -193,12 +235,15 @@
 
                 <div class="col-12">
                     <h2>
-                        Pricing Table
+                        <strong>
+                            Pricing Table
+                        </strong>
+                        
                     </h2>
                 </div>
 
                 <div class="col-12">
-                    <p>
+                    <p class="padding-bottom">
                         We can help you deliver and execute your future, wherever you compete, using the latest technologies, from strategy development to implementation.
                     </p>
                 </div>
@@ -209,11 +254,17 @@
                     class="col-4 single-card3">
 
                         <div class="container-up d-flex justify-content-between">
-                            <div class="icon"></div>
-                            <div class="icon"></div>
+                            <div>
+                                <p id="most-popular">Più popolare</p>
+                            </div>
+                            <div>
+                                <i class="bi bi-arrow-right-short"></i>
+                            </div>
                         </div>
                         <div class="d-flex justify-content-center">
-                            <div class="icon"></div>
+                            <div class="icon">
+                                {{ card3.icon }}
+                            </div>
                         </div>
                         
                         <div class="d-flex justify-content-center">
@@ -221,7 +272,7 @@
                                 {{ card3.title }}
                             </h3>
                         </div>
-                        <div class="d-flex justify-content-center">
+                        <div id="price" class="d-flex justify-content-center">
                             <h2>
                                 {{ card3.price }}
                             </h2>
@@ -230,9 +281,12 @@
                         <ul class="">
                             <li v-for="(list, index) in card3.list" class="d-flex justify-content-between align-items-center text-left">
                                 <div>
-                                    {{ list }}
+                                    {{ list.option }}
                                 </div>
-                                <div>icon</div>
+                                <div>
+                                    {{ list.check }}
+                                </div>
+                                
                             </li>
                         </ul>
                     </div>
@@ -256,6 +310,9 @@
     height: 50px;
     width: 50px;
     border: 1px dashed black;
+    img {
+        height: 100%;
+    }
 }
 
 //card
@@ -289,26 +346,36 @@
 }
 
 //card2
+.filter {
+    color: $color-one;
+}
 
 #container-card2 {
     background-color: #111117;
+    padding-bottom: 100px;
     h5 {
         color: $color-five;
+        padding-top: 100px;
     }
     h2{
-        padding-top: 10px;
+        padding-top: 20px;
+        padding-bottom: 10px;
         color: white;
     }
     h3{
+        padding-top: 10px;
+        padding-bottom: 20px;
         text-align: left;
+        color: white;
     }
     p {
         color: $color-two;
     }
+
 }
 .single-card2 {
     width: calc((100% / 2) - 30px);
-    border: 1px solid black;
+    background-color: #19191F;
     padding: 20px;
     margin: 15px;
 
@@ -321,17 +388,49 @@
 
 .single-card3 {
     width: calc((100% / 3) - 20px);
-    border: 1px solid black;
+    background-color: white;
     padding: 20px;
     margin: 10px;
+}
 
+.container-card3 {
+    background-color: #EEF4ED;
+    padding-bottom: 100px;
+    h5{
+        padding-top: 100px;
+        color: $color-five;
+    }
     h2 {
-        margin-bottom: 20px;
+        padding: 20px 0;
     }
 
     li {
         list-style: none;
+        margin: 0 20px;
+        padding: 10px;
+    }
+    h3 {
+        padding-top: 20px;
+    }
+    #most-popular {
+    padding-left: 20px;
+    padding-right: 20px;
+    background-color: $color-one;
+    }
+    .padding-bottom {
+        padding-bottom: 20px;
     }
 }
+   
+#price {
+    color: $color-one;
+}
+
+.bi-arrow-right-short {
+    color: $color-one;
+    font-size: 50px;
+}
+
+
 </style>
 
