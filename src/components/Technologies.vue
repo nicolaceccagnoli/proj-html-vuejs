a0a0dc037e7010cb5b95aa3a186e16ce5b7ff3cc
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-31 14:26:01 +0100

    Add Results and Technologies components

---
+++ src/components/Technologies.vue
@@ -0,0 +1,55 @@
+<script>
+    export default {
+        data() {
+            return {
+                technologies: [
+                    '/public/images/logo-1.png',
+                    '/public/images/logo-2.png',
+                    '/public/images/logo-3.png',
+                    '/public/images/logo-4.png',
+                    '/public/images/logo-5.png',
+                    '/public/images/logo-1.png'
+                ]
+            }
+            },
+            methods: {
+                },
+                mounted() {
+                },
+        }
+</script>
+
+
+<template>
+
+<div id="technologies">
+    <div class="my-container">
+        <div id="bottom-container">
+            <div class="row align-items-center">
+                <div 
+                v-for="singleTech in technologies"
+                class="tech-container col-2 d-flex justify-content-center">
+                    <img :src="singleTech" alt="#">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/scss/partials/variables.scss';
+@import '../assets/scss/partials/mixins.scss';
+
+#technologies {
+    background: rgb(14,27,42);
+    background: linear-gradient(77deg, rgba(14,27,42,1) 0%, rgba(23,58,81,1) 100%);
+    padding: 60px 0;
+    .tech-container img {
+        width: 100px;
+        filter: brightness(1000%);
+    }
+}
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,82 +3,36 @@ import Jumbotron from '../components/Jumbotron.vue';
 import Excellence from '../components/Excellence.vue';
 import TheCompany from '../components/TheCompany.vue';
 import ActionsProject from '../components/ActionsProject.vue';
+import Results from '../components/Results.vue';
+import Technologies from '../components/Technologies.vue';
 
     export default {
         data() {
             return {
-
-                // Creo un intervallo 
-                interval: 50,
-
-                goals: [
-                    {
-                        currentValue: 0,
-                        goal: 128,
-                        name: 'Certifications'
-                    },
-                    {
-                        currentValue: 0,
-                        goal: 230,
-                        name: 'Employees',
-                    },
-                    {
-                        currentValue: 0,
-                        goal: 517,
-                        name: 'Customers'
-                    },
-                    {
-                        currentValue: 0,
-                        goal: 94,
-                        name: 'Countries Served'
-                    }
-                ],
-                
                 technologies: [
-                    'logo-1.png',
-                    'logo-2.png',
-                    'logo-3.png',
-                    'logo-4.png',
-                    'logo-5.png',
-                    'logo-1.png'
+                    '/public/images/logo-1.png',
+                    '/public/images/logo-2.png',
+                    '/public/images/logo-3.png',
+                    '/public/images/logo-4.png',
+                    '/public/images/logo-5.png',
+                    '/public/images/logo-1.png'
                 ]
             }
-        },
-        methods: {
-            // Creo una funzione per aggiornare il contatore
-            updateGoal() {
-                 // Faccio un ciclo per controllare ogni singolo Goal in Goals
-                    for (let goal in this.goals) {
-                        const currentGoal = this.goals[goal];
-
-                        // Calcolo l'incremento per un effetto progressivo
-                        const increment = currentGoal.goal / (3000 / this.interval);
-
-                        // Imposto un intervallo per aggiornare gradualmente currentValue
-                        const goalInterval = setInterval(() => {
-                        this.goals[goal].currentValue += increment;
-
-                        // Arrotondo il valore verso il basso per rimuovere la parte decimale
-                        this.goals[goal].currentValue = Math.floor(this.goals[goal].currentValue);
-
-                        if (this.goals[goal].currentValue >= currentGoal.goal) {
-                            this.goals[goal].currentValue = currentGoal.goal;
-                            clearInterval(goalInterval);
-                        }
-                        }, this.interval);
-                    }
+            },
+            methods: {
                 },
-            },
-            mounted() {
-                setInterval(() => {
-                    this.updateGoal()
-                }, 1000);;
-            },
-            components:{
-                Jumbotron,
-                Excellence,
-                TheCompany,
-                ActionsProject
+                mounted() {
+                    setInterval(() => {
+                        this.updateGoal()
+                    }, 1000);;
+                },
+                components:{
+                    Jumbotron,
+                    Excellence,
+                    TheCompany,
+                    ActionsProject,
+                    Results,
+                    Technologies
             }
         }
 </script>
@@ -94,40 +48,9 @@ import ActionsProject from '../components/ActionsProject.vue';
 
     <ActionsProject />
 
-    <div id="results">
-        <div class="my-container">
-            <div id="up-container">
-                <div class="row flex-column justify-content-center text-center">
-                    <div class="col-auto">
-                        <h3>
-                            Results in <span> Numbers </span>
-                        </h3>
-                    </div>
-                    <div class="col-auto">
-                        <p>
-                            Our goal is to exceed expectations by delivering the berst job possible.
-                        </p>
-                    </div>
-                </div>
-                <div class="row justify-content-between">
-                    <div 
-                    v-for="(singleGoal, i) in goals"
-                    :key="i"
-                    class="goal text-success col text-center">
-                        <h3 class="goal-number">
-                            {{singleGoal.currentValue}}
-                        </h3>
-                        <p>
-                            {{ singleGoal.name }}
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div id="bottom-container">
+    <Results />
 
-            </div>
-        </div>
-    </div>
+    <Technologies />
 
 </template>
 
@@ -135,35 +58,4 @@ import ActionsProject from '../components/ActionsProject.vue';
 @import '../assets/scss/partials/variables.scss';
 @import '../assets/scss/partials/mixins.scss';
 
-#results {
-    background-image:
-        linear-gradient(77deg,
-         rgba(14,27,42,0.3), 
-         rgba(23,58,81,0.2)), 
-         url('/public/images/bg-7.jpg');
-    background-position: center;
-    padding: 100px 0;
-
-    h3 {
-        color: white;
-        font-size: 50px;
-        font-weight: bold;
-
-        span {
-            @include tertiary-span-style;
-        }
-
-    }
-
-    p {
-        color: #BDC0CD;
-        margin: 30px 0;
-    }
- 
-    .goal {
-        .goal-number {
-            color: $color-five;
-        }
-    }
-}
 </style>
