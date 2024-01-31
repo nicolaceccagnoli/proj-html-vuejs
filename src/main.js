767dd3857a26fd3c036de87e0eee6c423b2983e4
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-31 16:25:16 +0100

    message

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import { router } from './router.js'
-
-import App from './App.vue';
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap';
-
-createApp(App).use(router).mount('#app');
