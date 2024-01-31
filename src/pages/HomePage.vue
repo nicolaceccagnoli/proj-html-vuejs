1c9b9e21562724e782c322b4084afa2dceb46efb
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-31 12:50:23 +0100

    Results in progress

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -8,40 +8,71 @@ import ActionsProject from '../components/ActionsProject.vue';
         data() {
             return {
 
-                // Imposto una flag per il valore corrente dei singoli goal
-                currentValue: 0,
-
                 // Creo un intervallo 
                 interval: 50,
 
                 goals: [
                     {
+                        currentValue: 0,
                         goal: 128,
                         name: 'Certifications'
                     },
                     {
+                        currentValue: 0,
                         goal: 230,
                         name: 'Employees',
                     },
                     {
+                        currentValue: 0,
                         goal: 517,
                         name: 'Customers'
                     },
                     {
+                        currentValue: 0,
                         goal: 94,
                         name: 'Countries Served'
                     }
+                ],
+                
+                technologies: [
+                    'logo-1.png',
+                    'logo-2.png',
+                    'logo-3.png',
+                    'logo-4.png',
+                    'logo-5.png',
+                    'logo-1.png'
                 ]
             }
         },
         methods: {
             // Creo una funzione per aggiornare il contatore
             updateGoal() {
-                // Faccio un ciclo per controllare ogni singolo Goal in Goals
+                 // Faccio un ciclo per controllare ogni singolo Goal in Goals
+                    for (let goal in this.goals) {
+                        const currentGoal = this.goals[goal];
+
+                        // Calcolo l'incremento per un effetto progressivo
+                        const increment = currentGoal.goal / (3000 / this.interval);
+
+                        // Imposto un intervallo per aggiornare gradualmente currentValue
+                        const goalInterval = setInterval(() => {
+                        this.goals[goal].currentValue += increment;
+
+                        // Arrotondo il valore verso il basso per rimuovere la parte decimale
+                        this.goals[goal].currentValue = Math.floor(this.goals[goal].currentValue);
+
+                        if (this.goals[goal].currentValue >= currentGoal.goal) {
+                            this.goals[goal].currentValue = currentGoal.goal;
+                            clearInterval(goalInterval);
+                        }
+                        }, this.interval);
+                    }
                 },
             },
             mounted() {
-                this.updateGoal();
+                setInterval(() => {
+                    this.updateGoal()
+                }, 1000);;
             },
             components:{
                 Jumbotron,
@@ -65,30 +96,35 @@ import ActionsProject from '../components/ActionsProject.vue';
 
     <div id="results">
         <div class="my-container">
-            <div class="row flex-column justify-content-center text-center">
-                <div class="col-auto">
-                    <h3>
-                        Results in <span> Numbers </span>
-                    </h3>
+            <div id="up-container">
+                <div class="row flex-column justify-content-center text-center">
+                    <div class="col-auto">
+                        <h3>
+                            Results in <span> Numbers </span>
+                        </h3>
+                    </div>
+                    <div class="col-auto">
+                        <p>
+                            Our goal is to exceed expectations by delivering the berst job possible.
+                        </p>
+                    </div>
                 </div>
-                <div class="col-auto">
-                    <p>
-                        Our goal is to exceed expectations by delivering the berst job possible.
-                    </p>
+                <div class="row justify-content-between">
+                    <div 
+                    v-for="(singleGoal, i) in goals"
+                    :key="i"
+                    class="goal text-success col text-center">
+                        <h3 class="goal-number">
+                            {{singleGoal.currentValue}}
+                        </h3>
+                        <p>
+                            {{ singleGoal.name }}
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div class="row justify-content-between">
-                <div 
-                v-for="(singleGoal, i) in goals"
-                :key="i"
-                class="goal text-success col text-center">
-                    <h3 class="goal-number">
-                        {{currentValue}}
-                    </h3>
-                    <p>
-                        {{ singleGoal.name }}
-                    </p>
-                </div>
+            <div id="bottom-container">
+
             </div>
         </div>
     </div>
